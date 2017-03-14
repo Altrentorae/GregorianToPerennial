27deae04ae7baebad3b5491c65e6befe7dd74125
Altrentorae <killer39800@gmail.com>
2017-03-14 01:30:15 +0000

    VisStudioFiles

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GregorianToPerennial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateIN = new System.Windows.Forms.DateTimePicker();
+            this.outputBX = new System.Windows.Forms.Label();
+            this.sundayStrtTRUE = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // dateIN
+            // 
+            this.dateIN.Location = new System.Drawing.Point(12, 12);
+            this.dateIN.MaxDate = new System.DateTime(2100, 12, 31, 0, 0, 0, 0);
+            this.dateIN.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dateIN.Name = "dateIN";
+            this.dateIN.Size = new System.Drawing.Size(200, 20);
+            this.dateIN.TabIndex = 0;
+            this.dateIN.TabStop = false;
+            this.dateIN.Value = new System.DateTime(2017, 3, 13, 15, 49, 38, 0);
+            this.dateIN.ValueChanged += new System.EventHandler(this.dateIN_ValueChanged);
+            // 
+            // outputBX
+            // 
+            this.outputBX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputBX.Location = new System.Drawing.Point(12, 72);
+            this.outputBX.Name = "outputBX";
+            this.outputBX.Size = new System.Drawing.Size(199, 49);
+            this.outputBX.TabIndex = 1;
+            this.outputBX.Text = "Output";
+            this.outputBX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sundayStrtTRUE
+            // 
+            this.sundayStrtTRUE.AutoSize = true;
+            this.sundayStrtTRUE.Location = new System.Drawing.Point(12, 38);
+            this.sundayStrtTRUE.Name = "sundayStrtTRUE";
+            this.sundayStrtTRUE.Size = new System.Drawing.Size(136, 17);
+            this.sundayStrtTRUE.TabIndex = 2;
+            this.sundayStrtTRUE.Text = "Week Start on Monday\r\n";
+            this.sundayStrtTRUE.UseVisualStyleBackColor = true;
+            this.sundayStrtTRUE.CheckedChanged += new System.EventHandler(this.sundayStrtTRUE_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(229, 130);
+            this.Controls.Add(this.sundayStrtTRUE);
+            this.Controls.Add(this.outputBX);
+            this.Controls.Add(this.dateIN);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gregorian to IFC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateIN;
+        private System.Windows.Forms.Label outputBX;
+        private System.Windows.Forms.CheckBox sundayStrtTRUE;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GregorianToPerennial
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            outputBX.Font = new Font("Sans Serif", 10);
+            dateIN.Value = DateTime.Today;
+            Main();
+        }
+        private TimeSpan findDiff(DateTime x, DateTime y)
+        {
+            return (y - x);
+        }
+        private string getPRDay(int x, bool q)
+        {
+            string y = "";
+            x = x % 7;
+            switch(x)
+            {
+                case 1:
+                    if (q)
+                    {
+                        y = "Monday";
+                    }
+                    else
+                    {
+                        y = "Sunday";
+                    }
+                    break;
+                case 2:
+                    if (q)
+                    {
+                        y = "Tuesday";
+                    }
+                    else
+                    {
+                        y = "Monday";
+                    }
+                    break;
+                case 3:
+                    if (q)
+                    {
+                        y = "Wednesday";
+                    }
+                    else
+                    {
+                        y = "Tuesday";
+                    }
+                    break;
+                case 4:
+                    if (q)
+                    {
+                        y = "Thursday";
+                    }
+                    else
+                    {
+                        y = "Wednesday";
+                    }
+                    break;
+                case 5:
+                    if (q)
+                    {
+                        y = "Friday";
+                    }
+                    else
+                    {
+                        y = "Thursday";
+                    }
+                    break;
+                case 6:
+                    if (q)
+                    {
+                        y = "Saturday";
+                    }
+                    else
+                    {
+                        y = "Friday";
+                    }
+                    break;
+                case 0:
+                    if (q)
+                    {
+                        y = "Sunday";
+                    }
+                    else
+                    {
+                        y = "Saturday";
+                    }
+                    break; 
+            }
+            return y;
+        }
+        private void dateIN_ValueChanged(object sender, EventArgs e)
+        {
+            Main();
+        }
+        private void Main()
+        {
+            string prDateDay, prDateDate = "", prDateMonth = "";            
+            string prDateYear = dateIN.Value.Year.ToString();
+            DateTime date = dateIN.Value;
+            DateTime jan1 = new DateTime(dateIN.Value.Year, 1, 1, 0, 0, 0);
+            TimeSpan diff = findDiff(date, jan1);
+            int diffDays = (int)diff.TotalDays;
+            diffDays = (diffDays * -1) + 1;
+            System.Diagnostics.Debug.Write("diffDays: " + diffDays);
+
+            bool sndTRUE;
+            if (sundayStrtTRUE.Checked)
+            {
+                sndTRUE = true;
+            }
+            else
+            {
+                sndTRUE = false;
+            }
+            prDateDay = getPRDay(diffDays, sndTRUE);
+
+            if (diffDays < 29)
+            {
+                prDateDate = diffDays.ToString();
+                prDateMonth = "January";
+            }
+            if (diffDays > 28 && diffDays < (28 * 2) + 1)
+            {
+                prDateDate = (diffDays - 28).ToString();
+                prDateMonth = "February";
+            }
+            if (diffDays > 28 * 2 && diffDays < (28 * 3) + 1)
+            {
+                prDateDate = (diffDays - 28 * 2).ToString();
+                prDateMonth = "March";
+            }
+            if (diffDays > 28 * 3 && diffDays < (28 * 4) + 1)
+            {
+                prDateDate = (diffDays - 28 * 3).ToString();
+                prDateMonth = "April";
+            }
+            if (diffDays > 28 * 4 && diffDays < (28 * 5) + 1)
+            {
+                prDateDate = (diffDays - 28 * 4).ToString();
+                prDateMonth = "May";
+            }
+            if (diffDays > 28 * 5 && diffDays < (28 * 6) + 1)
+            {
+                prDateDate = (diffDays - 28 * 5).ToString();
+                prDateMonth = "June";
+            }
+            if (diffDays > 28 * 6 && diffDays < (28 * 7) + 1)
+            {
+                prDateDate = (diffDays - 28 * 6).ToString();
+                prDateMonth = "Sol";
+            }
+            if (diffDays > 28 * 7 && diffDays < (28 * 8) + 1)
+            {
+                prDateDate = (diffDays - 28 * 7).ToString();
+                prDateMonth = "July";
+            }
+            if (diffDays > 28 * 8 && diffDays < (28 * 9) + 1)
+            {
+                prDateDate = (diffDays - 28 * 8).ToString();
+                prDateMonth = "August";
+            }
+            if (diffDays > 28 * 9 && diffDays < (28 * 10) + 1)
+            {
+                prDateDate = (diffDays - 28 * 9).ToString();
+                prDateMonth = "September";
+            }
+            if (diffDays > 28 * 10 && diffDays < (28 * 11) + 1)
+            {
+                prDateDate = (diffDays - 28 * 10).ToString();
+                prDateMonth = "October";
+            }
+            if (diffDays > 28 * 11 && diffDays < (28 * 12) + 1)
+            {
+                prDateDate = (diffDays - 28 * 11).ToString();
+                prDateMonth = "November";
+            }
+            if (diffDays > 28 * 12 && diffDays < (28 * 13) + 1)
+            {
+                prDateDate = (diffDays - 28 * 12).ToString();
+                prDateMonth = "December";
+            }
+            if (diffDays == 365 || diffDays == 366)
+            {
+                prDateDay = "";
+                prDateDate = "Year";
+                prDateMonth = "Day";
+            }
+            // OUTPUT
+            outputBX.Text = string.Format("{0} {1} {2} {3}", prDateDay, prDateDate, prDateMonth, prDateYear);
+        }
+
+        private void sundayStrtTRUE_CheckedChanged(object sender, EventArgs e)
+        {
+            Main();
+        }
+    }
+}
